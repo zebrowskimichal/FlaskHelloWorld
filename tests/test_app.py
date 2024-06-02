1f7b93bc6d609d4f03eb359c26d407cbf4d9889c
Michał Żebrowski <117666366+zebrowskimichal@users.noreply.github.com>
2024-06-02 21:11:53 +0200

    test test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,10 @@
-from FlaskHelloWorld import app
+import os
+import sys
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+from app import app
 client = app.test_client()
 def test():
     response = client.get("/")
