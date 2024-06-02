54c39d83281b70a2afa648e70c407562b9abf43c
Michal Zebrowski <zebrowski.michal02@gmail.com>
2024-06-02 20:41:04 +0200

    Created app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
