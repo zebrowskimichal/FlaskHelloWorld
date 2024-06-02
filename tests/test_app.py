99a79750755619c3519a740e36a45bca8f792d26
Michał Żebrowski <117666366+zebrowskimichal@users.noreply.github.com>
2024-06-02 21:15:09 +0200

    Repair test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,10 +2,10 @@ import os
 import sys
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir)
+sys.path.insert(0, parent_dir) #code enables pytest to import the app.py
 
 from app import app
 client = app.test_client()
 def test():
     response = client.get("/")
-    assert response.status_code == 200 #request has been succeded
+    assert response.status_code == 200 #request has been succeeded
